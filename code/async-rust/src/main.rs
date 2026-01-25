835de7ef57a2519c6b215b39417a6b9bfb630645
David Chubabriya <david.chubabriya@gmail.com>
2026-01-25 20:04:11 +0000

    docs(async): update async code example to include pin

--- code/async-rust/src/main.rs
+++ code/async-rust/src/main.rs
@@ -1,17 +1,57 @@
-use std::{pin::Pin, task::{Context}};
+use std::{pin::Pin, task::{Context, Poll}};
+use std::future::Future;
 
-pub enum Poll<T> {
-    Ready(T),
-    Pending
+// pub enum PollOwn<T> {
+//     Ready(T),
+//     Pending
+// }
+// 
+// pub trait FutureOwn {
+//     type Output;
+//     fn poll(self: Pin<&mut Self>, context: &mut Context) -> PollOwn<Self::Output>;
+// }
+
+struct CharCounter<F> {
+    inner: F
 }
 
-pub trait Future {
-type Output;
-    fn poll(self: Pin<&mut Self>, context: &mut Context) -> Poll<Self::Output>;
+impl<F> Future for CharCounter<F>
+where
+    F: Future<Output = String>
+{
+    type Output = usize;
+
+    // Pin, a wrapper type guarantees that the value inside cannot be moved in memory.
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        // Cannot move out of a pinned field so need to safely project 
+        // the pin to inner future.
+        let inner: Pin<_> = unsafe { self.as_mut().map_unchecked_mut(|s| &mut s.inner) };
+
+        // Delegate polling to inner future
+        // a. If/when the future is ready count the number of characters
+        // b. Else pending
+        let p = match inner.poll(cx) {
+            Poll::Ready(s) => Poll::Ready(s.chars().count()),
+            Poll::Pending => Poll::Pending
+        };
+        p
+    }
 }
 
-// Future Combinators - TODO
+// Future Combinators.
+// These allow to chain futures together.
+// Methods like map() make it possible.
+fn count_chars(value: impl Future<Output = String>) -> impl Future<Output = usize> {
+    CharCounter {
+        inner: value
+    }
+}
+
+async fn some_s() -> String {
+    "aha".to_string()
+}
 
 fn main() {
-    println!("Hello, world!");
+    let _a = count_chars(some_s());
 }
+
